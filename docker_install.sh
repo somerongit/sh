9de56b7626b941ee0117669b03ac712a510bf3b8
Someron Bakuli <68781900+somerongit@users.noreply.github.com>
2024-07-09 17:36:07 +0530

    init

---
+++ docker_install.sh
@@ -0,0 +1,22 @@
+echo "Docker Installation Started"
+echo "Pkg Update"
+apt update
+
+apt install apt-transport-https ca-certificates curl software-properties-common
+
+echo "Fetching Dcoker Insatller Started"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+echo "Fetching Dcoker Insatller Done"
+
+apt-cache policy docker-ce
+
+echo "Dokcer Installation Started"
+apt install docker-ce
+echo "Dokcer Installation Done"
+
+sudo systemctl status docker
