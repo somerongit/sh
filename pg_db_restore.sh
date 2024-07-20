c72da6849571c4742f2bd93c1054f2e1929a25a0
Someron Bakuli <68781900+somerongit@users.noreply.github.com>
2024-07-20 21:30:47 +0530

    Update pg_db_restore.sh

--- pg_db_restore.sh
+++ pg_db_restore.sh
@@ -7,3 +7,6 @@ BACKUP_FILE="./backup.gz"
 gunzip $BACKUP_FILE
 docker exec -i $CONTAINER_NAME psql -U $DB_USER $RESTORE_DB_NAME < ./backup
 echo "Database restored from $BACKUP_FILE"
+
+#  Backup 
+#  docker exec -t $DB_CONTAINER_NAME pg_dump -U $DB_USER $DB_BACKUP_NAME | gzip > /home/user/app/backup/app_db_dump_`date +%d-%m-%Y_%H-%M-%S`.gz
