2696b72d2d672491f407a9eb4bd898ad8dca7879
Someron Bakuli <68781900+somerongit@users.noreply.github.com>
2024-09-05 11:44:27 +0530

    fix

--- jenkins.sh
+++ jenkins.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "Installing Jenkins Started"
+# echo "Installing Jenkins Started"
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 
-sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+# sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-echo "Jenkins Insatalling..."
+# echo "Jenkins Insatalling..."
 
-apt install jenkins
+# apt install jenkins
 
-echo "Starting Jenkins"
+# echo "Starting Jenkins"
 
-systemctl start jenkins.service
-systemctl status jenkins
+# systemctl start jenkins.service
+# systemctl status jenkins
 
-echo "Jenkins Installation Done"
+# echo "Jenkins Installation Done"
